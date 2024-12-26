b7460d7c1c3dd7f2b277f2a6d6bdb0e37c7a38ed
usman <usmanjamil876@gmail.com>
2024-12-26 11:52:57 +0500

    Preview Page updated

---
+++ preview.js
@@ -0,0 +1,38 @@
+let accountType = localStorage.getItem("Account Type");
+document.getElementById("accounttype").value = accountType;
+
+
+let fName = localStorage.getItem("First Name");
+document.getElementById("firstname").value = fName;
+
+
+let lName = localStorage.getItem("Last Name");
+document.getElementById("lastname").value = lName;
+
+
+let sl = localStorage.getItem("Monthly Salary");
+document.getElementById("salary").value = sl;
+
+let phoneNumber = localStorage.getItem("Phone Number");
+document.getElementById("phonenumber").value = phoneNumber;
+
+let cni = localStorage.getItem("CNIC NO.");
+document.getElementById("cnic").value = cni;
+
+let st = localStorage.getItem("Street Address");
+document.getElementById("street").value = st;
+
+let st2 = localStorage.getItem("Street Address line 2");
+document.getElementById("street-2").value = st2;
+
+let city = localStorage.getItem("City");
+document.getElementById("city").value = city;
+
+let region = localStorage.getItem("Region");
+document.getElementById("region").value = region;
+
+let zipCode = localStorage.getItem("Postal/Zip Code");
+document.getElementById("zidcode").value = zipCode;
+
+let cntr = localStorage.getItem("Country");
+document.getElementById("country").value = cntr;
